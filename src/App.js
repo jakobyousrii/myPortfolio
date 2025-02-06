fd9c0c0c2502237274bb1eda492a078a636ad4fc
jakobyousrii <jakobyousri1@gmail.com>
2025-02-06 16:35:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Fragment } from "react/cjs/react.production.min";
 import Layout from "./components/Layout/Layout";
 import About from "./components/Main/About";
-import React, { useRef,useState } from "react";
+import React, { useRef, useState } from "react";
 import Skills from "./components/Main/Skills";
 import Portfolios from "./components/Main/Portfolios/Portfolios";
 import Contact from "./components/Main/Contact/Contact";
 import Footer from "./components/Layout/Footer";
-import {linkNavPrep} from "./assets/linkNav";
+import { linkNavPrep } from "./assets/linkNav";
 
 function App() {
   const [isClicked, setIsClicked] = useState(false);
@@ -19,9 +19,12 @@ function App() {
   const aboutLinkHandler = linkNavPrep(aboutNavigateRef, setIsClicked);
   const contactLinkHandler = linkNavPrep(contactNavigateRef, setIsClicked);
 
+  //Hamburger Button
   const hamburgerclickHandler = () => {
-      setIsClicked(previos => { return !previos });
-  }
+    setIsClicked((previos) => {
+      return !previos;
+    });
+  };
 
   return (
     <Fragment>
@@ -31,13 +34,13 @@ function App() {
         portfolioRef={portfolioLinkHandler}
         skillsRef={skillsLinkHandler}
         aboutRef={aboutLinkHandler}
-        contactRef={contactLinkHandler}>
-
+        contactRef={contactLinkHandler}
+      >
         <About ref={aboutNavigateRef} />
         <Skills ref={skillsNavigateRef} />
         <Portfolios ref={portfoliosNavigateRef} />
         <Contact ref={contactNavigateRef} />
-        <Footer/>
+        <Footer />
       </Layout>
     </Fragment>
   );
